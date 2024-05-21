e37084f4a21947f165f859e5488d51a9c79cbcc2
akorzunin <akorzunin123@gmail.com>
2024-05-21 15:28:32 +0300

    feat: added waybar config

---
+++ install.sh
@@ -0,0 +1,6 @@
+cd ~/.config/
+
+ln -sf /home/avkorz/Documents/hyprconf/hypr hypr
+ln -sf /home/avkorz/Documents/hyprconf/rofi rofi
+ln -sf /home/avkorz/Documents/hyprconf/kitty kitty
+ln -sf /home/avkorz/Documents/hyprconf/waybar waybar
