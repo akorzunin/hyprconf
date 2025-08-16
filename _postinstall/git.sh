6a104c4a96165828e6f6ec322b04493ee9322e88
akorzunin <akorzunin123@gmail.com>
2025-08-16 19:43:28 +0300

    added git setup

---
+++ _postinstall/git.sh
@@ -0,0 +1,6 @@
+git config --global user.email "..."
+git config --global user.name "..."
+git config --global gpg.format ssh
+git config --global commit.gpgSign true
+git config --global user.signingkey ~/.ssh/id_ed25519
+gh auth login
