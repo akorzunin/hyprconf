1ab514a6bff52a2323358e3fefe8ef4d6e692036
akorzunin <akorzunin123@gmail.com>
2025-09-05 01:00:09 +0300

    add autologin script

---
+++ _postinstall/autologin.sh
@@ -0,0 +1,9 @@
+sudo systemctl disable ly
+
+sudo mkdir -p /etc/systemd/system/getty@tty1.service.d
+sudo tee /etc/systemd/system/getty@tty1.service.d/override.conf >/dev/null <<EOF
+[Service]
+ExecStart=
+ExecStart=-/sbin/agetty --autologin $USER --noclear %I $TERM
+EOF
+
