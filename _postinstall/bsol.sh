596a0de787359fedda3ad5186876dd56e59b4ada
akorzunin <akorzunin123@gmail.com>
2025-10-19 15:31:43 +0300

    upd grub settings

--- _postinstall/bsol.sh
+++ _postinstall/bsol.sh
@@ -1,15 +1,19 @@
+#!/bin/bash
+mkdir -p ~/build
+cd ~/build/
 git clone https://github.com/harishnkr/bsol.git
 cd bsol
 sudo cp -r bsol/ /boot/grub/themes/bsol
 
-# /etc/default/grub
+# sudo nvim /etc/default/grub
 #
 # GRUB_DEFAULT=saved
+# GRUB_TIMEOUT=1
 # GRUB_THEME="/boot/grub/themes/bsol/theme.txt"
 # GRUB_SAVEDEFAULT=true
 # GRUB_DISABLE_OS_PROBER=false
 
-# /boot/grub/themes/bsol/theme.txt
+# sudo nvim /boot/grub/themes/bsol/theme.txt
 #
 # terminal-font: "Victor Mono Regular 16"
 #
