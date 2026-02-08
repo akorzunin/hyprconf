a69124e7fe7d1cab37493dbf8f4759ade9ff59a3
akorzunin <akorzunin123@gmail.com>
2026-02-08 21:17:08 +0300

    feat: added default mime types to open w/ mpv

---
+++ _postinstall/xdg_mimeapps.sh
@@ -0,0 +1,7 @@
+xdg-mime default mpv.desktop video/mp4
+xdg-mime default mpv.desktop video/webm
+xdg-mime default mpv.desktop video/mpeg
+xdg-mime default mpv.desktop video/quicktime
+xdg-mime default mpv.desktop video/x-matroska
+xdg-mime default mpv.desktop video/x-msvideo
+xdg-mime default mpv.desktop video/ogg
